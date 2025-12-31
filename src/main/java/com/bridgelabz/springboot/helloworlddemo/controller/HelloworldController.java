aeb117563bd181af5a414fc223cb115410c479a4
BL-maheswari <ipperu.maheswari@bridgelabz.com>
2025-12-31 12:21:10 +0530

    final commit

--- src/main/java/com/bridgelabz/springboot/helloworlddemo/controller/HelloworldController.java
+++ src/main/java/com/bridgelabz/springboot/helloworlddemo/controller/HelloworldController.java
@@ -3,8 +3,15 @@ package com.bridgelabz.springboot.helloworlddemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.springboot.helloworlddemo.model.User;
 
 //@Controller
 @RestController
@@ -26,6 +33,31 @@ public class HelloworldController {
 	@RequestMapping("/")
 	public String hello() {
 		return "hello";
+	}
+
+	@GetMapping
+	public String sayHello() {
+		return "Hello from BridgeLabz";
+	}
+
+	@GetMapping("/query")
+	public String sayHelloQuery(@RequestParam String name) {
+		return "Hello " + name + " from BridgeLabz";
+	}
+
+	@GetMapping("/param/{name}")
+	public String sayHelloPath(@PathVariable String name) {
+		return "Hello " + name + " from BridgeLabz";
+	}
+
+	@PostMapping("/post")
+	public String sayHelloPost(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+	}
+
+	@PutMapping("/put/{firstName}")
+	public String sayHelloPut(@PathVariable String firstName, @RequestParam String lastName) {
+		return "Hello " + firstName + " " + lastName + " from BridgeLabz";
 	}
 
 }
---
+++ src/main/java/com/bridgelabz/springboot/helloworlddemo/model/User.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.springboot.helloworlddemo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	private int id;
+	private String firstName;
+	private String lastName;
+
+	public User() {
+	}
+
+	public User(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
